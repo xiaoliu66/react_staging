38d02b736ece8669b75c22d42d352077a92d3ad7
xiaoliu66 <lunhuilk@163.com>
2021-05-29 16:20:55 +0800

    完成Switch的使用

---
+++ 11_src_Switch的使用/App.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./pages/Home"; // Home是路由组件
+import About from "./pages/About"; // About是路由组件
+import Header from "./components/Header"; // Header 是一般组件
+import MyNavLink from "./components/MyNavLink"; //MyNavLink是一般组件
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-xs-offset-2 col-xs-8">
+                        <div className="page-header">
+                            <Header />
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-2 col-xs-offset-2">
+                        <div className="list-group">
+                            {/* 原生html中，靠<a>跳转不同的页面 */}
+                            {/* <a className="list-group-item" href="./about.html">
+                                About
+                            </a>
+                            <a className="list-group-item active" href="./home.html">
+                                Home
+                            </a> */}
+                            {/* 在react中靠路由链接实现切换组件 */}
+
+                            {/* <NavLink className="list-group-item" to="/about">
+                                About
+                            </NavLink>
+                            <NavLink className="list-group-item" to="/home">
+                                Home
+                            </NavLink> */}
+                            <MyNavLink to="/about">About</MyNavLink>
+                            <MyNavLink to="/home">Home</MyNavLink>
+                        </div>
+                    </div>
+                    <div className="col-xs-6">
+                        <div className="panel">
+                            <div className="panel-body">
+                                {/* 注册路由 */}
+                                <Switch>
+                                    <Route path="/about" component={About} />
+                                    <Route path="/home" component={Home} />
+                                </Switch>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
