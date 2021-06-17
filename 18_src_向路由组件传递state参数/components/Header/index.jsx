03630d5ce3473db56376d99686015ebb4f58b6f6
xiaoliu66 <lunhuilk@163.com>
2021-06-17 21:31:29 +0800

    格式化

--- 18_src_向路由组件传递state参数/components/Header/index.jsx
+++ 18_src_向路由组件传递state参数/components/Header/index.jsx
@@ -1,11 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Header extends Component {
     render() {
-        return (
-            <div>
-                Hhhh
-            </div>
-        )
+        return <div>Hhhh</div>;
     }
 }
