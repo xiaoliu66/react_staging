d6300b175b75b37dd62c68b3291c7d76818183a5
xiaoliu66 <lunhuilk@163.com>
2021-06-18 21:20:29 +0800

    格式化代码

--- 18_src_向路由组件传递state参数/components/MyNavLink/index.jsx
+++ 18_src_向路由组件传递state参数/components/MyNavLink/index.jsx
@@ -3,6 +3,6 @@ import { NavLink } from "react-router-dom";
 
 export default class MyNavLink extends Component {
     render() {
-        return <NavLink className="list-group-item" {...this.props}/>
+        return <NavLink className="list-group-item" {...this.props} />;
     }
 }
