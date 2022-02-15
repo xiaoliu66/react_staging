1c910d0b1e39314a47ed4411f586f5c997698071
xiaoliu66 <lunhuilk@163.com>
2022-02-15 22:55:38 +0800

    [update]：有关插件及使用

---
+++ 02_src_hello_react/components/Test/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+  render() {
+    return (
+      <div>index</div>
+    )
+  }
+}
+
+// 输入rcc  按下回车  出现上述代码  需要安装 ES7+ React/Redux/React-Native snippets 插件才能实现。
